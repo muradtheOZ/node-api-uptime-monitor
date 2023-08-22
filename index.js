d55fd6fa6305a699454d6011e880e403e9072471
muradtheOZ <muradahmed199771@gmail.com>
2023-08-22 20:05:26 +0600

    index file changed

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const data = require("./lib/data");
 // app object scaffolding
 const app = {};
 
+// data.update('test','anotherFile',{'name':'Hello','relegion':'Programming'},(err)=>{
+//   console.log("I am error", err);
+// })
+
+// data.create('test','anotherFile',{'hello':'hello world','programming langauage':'nodeJs'},(err)=>{
+//   console.log('I am the error',err)
+// })
+
 data.read("test", "newFile", (err, data) => {
   console.log("data is", data, err);
 });
+
+// data.delete('test','anotherFile',(err)=>{
+//   console.log(err)
+// })
 
 // create server
 app.createServer = () => {
